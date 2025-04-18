010f97a076d2bb962a4f53c6ecf2a23d94988217
GamithaGimhana <gamitha.gimhana99@gmail.com>
2025-04-18 16:45:58 +0530

    first commit

---
+++ style_js/case-02-script.js
@@ -0,0 +1,16 @@
+let colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+
+const updateColors = () => {
+    // last color ek allagnnw
+    let lastColor = colors.pop();
+    // mul color ekt last color ek set krnw
+    colors.unshift(lastColor);
+
+    // array eke content ek maru krnw
+    colors.map((item,index) => {document.getElementsByTagName("div")[index].style.backgroundColor = item});
+
+    console.log(colors);
+}
+
+// function ekt intervalekk denw
+setInterval(updateColors, 3000);
